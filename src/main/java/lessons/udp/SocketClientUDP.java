7f409eab92a5b6df4c5c382ac8fa2e04dd411421
Ruma <matteoh8@hotmail.it>
2025-05-13 11:25:24 +0200

    udp ok

--- src/main/java/lessons/udp/SocketClientUDP.java
+++ src/main/java/lessons/udp/SocketClientUDP.java
@@ -1,4 +1,36 @@
 package lessons.udp;
 
+import java.net.*;
+
 public class SocketClientUDP {
+    public static void main(String[] args) {
+        try {
+            UdpEchoClient.createUdpClient();
+        } catch (Exception e) {
+            System.err.println("Errore: " + e.getMessage());
+        }
+    }
+}
+
+class UdpEchoClient {
+    public static void createUdpClient() throws Exception {
+        DatagramSocket socket = new DatagramSocket();
+        String message = "Hello UDP dal client!";
+        byte[] data = message.getBytes();
+
+        // Invia il messaggio al server
+        socket.connect(InetAddress.getByName("localhost"), 8888);
+        DatagramPacket packet = new DatagramPacket(data, data.length);
+        socket.send(packet);
+
+        // Ricezione della risposta
+        byte[] buffer = new byte[1024];
+        DatagramPacket inPacket = new DatagramPacket(buffer, buffer.length);
+        socket.receive(inPacket);
+
+        String echo = new String(inPacket.getData(), 0, inPacket.getLength());
+        System.out.println("Echo dal server: " + echo);
+
+        socket.close();
+    }
 }
--- src/main/java/lessons/udp/SocketServerUDP.java
+++ src/main/java/lessons/udp/SocketServerUDP.java
@@ -1,4 +1,47 @@
 package lessons.udp;
 
+import java.net.*;
+
 public class SocketServerUDP {
+
+    public static void main(String[] args) {
+        try {
+            UdpEchoServer.createUdpServer();
+        } catch (Exception e) {
+            System.err.println("Errore: " + e.getMessage());
+        }
+    }
+}
+
+class UdpEchoServer {
+    public static void createUdpServer() throws Exception {
+        DatagramSocket socket = new DatagramSocket(8888);
+        byte[] buffer = new byte[1024];
+        System.out.println("UDP Echo Server Started\r\n");
+
+        // Timeout di 10 secondi. Poi la connessione si chiude se non ci sono dati
+        socket.setSoTimeout(10000);
+
+        while (true) {
+            try {
+                DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+                socket.receive(request);
+
+                String received = new String(request.getData(), 0, request.getLength());
+                System.out.println("Ricevuto: " + received);
+
+                // Echo back: invia il pacchetto ricevuto al mittente
+                DatagramPacket response = new DatagramPacket(
+                        request.getData(), request.getLength(),
+                        request.getAddress(), request.getPort()
+                );
+                socket.send(response);
+            } catch (SocketTimeoutException e) {
+                System.out.println("Timeout scaduto. Chiudo il server.");
+            } catch (Exception e) {
+                System.err.println("Errore: " + e.getMessage());
+            }
+            break;
+        }
+    }
 }
